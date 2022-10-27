e96a64119367546f4798e04717eb9adc5905ed7a
Rio <riobeggs1@gmail.com>
2022-10-28 12:52:35 +1300

    added doc strings

--- examples/unittest_practice.py
+++ examples/unittest_practice.py
@@ -5,33 +5,43 @@ class TestExamples(unittest.TestCase):
     """Common testing unit testing methods"""
 
     def test_is_equal(self):
+        """Tests that a == b"""
         pass
 
     def test_is_not_equal(self):
+        """Tests that a != b"""
         pass
 
     def test_is_true(self):
+        """Tests that a == True"""
         pass
 
     def test_is_false(self):
+        """Tests that a == False"""
         pass
 
     def test_is_none(self):
+        """Tests that a == None"""
         pass
 
     def test_is_not_none(self):
+        """Tests that a != None"""
         pass
 
     def test_is_in(self):
+        """Tests that a is in ["a", "b", "c"]"""
         pass
 
     def test_is_not_in(self):
+        """Tests that a is not in ["x", "y", "z"]"""
         pass
 
     def test_is_instance(self):
+        """Tests that a is an instance of an integer"""
         pass
 
     def test_is_not_instance(self):
+        """Tests that a is an instance of a string"""
         pass
 
 
