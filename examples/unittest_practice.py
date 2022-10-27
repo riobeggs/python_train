c500818057959f1dcf01d3b3bdead8c12f2a5fbe
Rio <riobeggs1@gmail.com>
2022-10-28 12:47:57 +1300

    setup structure for unittesting practice

---
+++ examples/unittest_practice.py
@@ -0,0 +1,39 @@
+import unittest
+
+
+class TestExamples(unittest.TestCase):
+    """Common testing unit testing methods"""
+
+    def test_is_equal(self):
+        pass
+
+    def test_is_not_equal(self):
+        pass
+
+    def test_is_true(self):
+        pass
+
+    def test_is_false(self):
+        pass
+
+    def test_is_none(self):
+        pass
+
+    def test_is_not_none(self):
+        pass
+
+    def test_is_in(self):
+        pass
+
+    def test_is_not_in(self):
+        pass
+
+    def test_is_instance(self):
+        pass
+
+    def test_is_not_instance(self):
+        pass
+
+
+if __name__ == "__main__":
+    unittest.main()
